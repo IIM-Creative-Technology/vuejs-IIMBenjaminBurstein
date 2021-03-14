e4552c118558b8000fe27efc07787dc55e59ae67
Alexandre Caramel <caramel.alexandre@gmail.com>
2021-03-14 13:53:41 +0100

    :rocket:

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -2,13 +2,14 @@
 <h1>Blog</h1>
 <div v-for="(post, index) in posts" :key="(post, index)">
     <router-link :to=" '/blog/'+index" >
-        <div class="row content">
+        <div class="list">
             <div class="col-lg-4 "> 
                 <img :src="post.img" class="image">
             </div>
-            <h2>{{post.title}}</h2>
-            <div class="col-lg-8">
-                 <p>{{post.content}}</p>
+            
+            <div class="col-lg-8" id="contentlist">
+                <h2>{{post.title}}</h2>
+                <p>{{post.content}}</p>
             </div>   
         </div>
     </router-link>
@@ -25,13 +26,27 @@ export default {
    
 </script>
 <style scoped>
+*{
+    text-decoration: none;
+    color: black;
+}
     .image{
         width: 90%;
         height: auto;
     }
-     .content{
+     .list{
         border: solid black 1px;
         max-width: 75%;
         margin: 25px auto;
+        display: flex;
+        align-items: center;
     }   
+    #contentlist h2{
+        font-size: 34px;
+        font-weight: bold;
+    }
+    #contentlist p {
+        font-size: 16px;
+        font-weight: 400;
+    }
 </style>
